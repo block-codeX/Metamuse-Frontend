ce7979e2e5c937904a5d6802499d57a156045950
2022phyro <phyrokelstein2@gmail.com>
2025-06-09 04:50:46 +0100

    Added a visual cue to let users know when their work was being synced

--- metamuse/src/app/projects/studio/[projectId]/canvas/components/major-tool-bars/top-side-tools.tsx
+++ metamuse/src/app/projects/studio/[projectId]/canvas/components/major-tool-bars/top-side-tools.tsx
@@ -3,19 +3,26 @@ import Commands from "../commands/command";
 import { CanvasOrientationSwitcher } from "../orientation/tools/switcher";
 import { useCanvasSync } from "../contexts/canvas-sync-context";
 import { useEffect, useState } from "react";
+import { FlashingText } from "@/components/ui/flashing-text";
 export default function TopSideTools() {
-  const {initialized} = useCanvasSync()
-  const [loaded, setLoaded] = useState(false)
+  const { initialized } = useCanvasSync();
+  const [loaded, setLoaded] = useState(false);
   useEffect(() => {
-    console.log("Current initialization", initialized)
-    setLoaded(initialized)
-  }, [initialized])
-  // const[isLoaded, setIsLoaded] 
+    console.log("Current initialization", initialized);
+    setLoaded(initialized);
+  }, [initialized]);
+  // const[isLoaded, setIsLoaded]
   // useE
   return (
     <div className="flex flex-row items-center justify-start bg-background space-y-2 border h-full overflow-x-auto overflow-y-hidden">
-      <div className={`w-4 h-4 mx-4 rounded-full self-start mt-3  ${loaded == true ? "bg-success": "bg-error"}`}
-      ></div>
+      <div className="w-8 h-8 mx-4 self-start mt-3 flex flex-col items-start border border-red-500">
+        <div
+          className={`w-4 h-4 rounded-full ${
+            loaded === true ? "bg-success" : "bg-error"
+          }`}
+        ></div>
+        {loaded && <FlashingText text="Art Synced" color="text-success" />}
+      </div>
       <Commands />
       <Strokes />
       <CanvasOrientationSwitcher />
